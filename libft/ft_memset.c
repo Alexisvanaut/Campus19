7d206d224209a1f70ac2c1efe57de6817284bb87
Alexisvanaut <169139826+Alexisvanaut@users.noreply.github.com>
2025-03-27 15:05:32 +0100

    libft day 1

---
+++ libft/ft_memset.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void    *memset(void *b, int c, size_t len)
+{
+    size_t      i;
+    unsigned char   *s2;
+
+    i = 0;
+    s2 = b;
+    while (i < len)
+    {
+        s2[i] = c;
+        i++;
+    return (b);
+}
